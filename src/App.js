f0c2320f32252540a09af532e4f66e20ff179a8f
Jean Carlo <jeancarlo@Host-006.home>
2023-05-21 21:43:57 +0200

    Inicio del proyecto pet

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css'
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+
+function App() {
+  return (
+    <>
+      <Home/ >
+      <Sobre/ >
+    </>
+  );
+}
+
+export default App;
